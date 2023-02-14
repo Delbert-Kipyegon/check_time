c5ee400b2bc2b3e2be3e0f01819583790c141173
Delbert-Kipyegon <delbertkip@gmail.com>
2023-02-14 11:44:59 +0300

    second

--- src/testPages/covid.jsx
+++ src/testPages/covid.jsx
@@ -1,43 +1,84 @@
 import React from 'react'
 import './covid.css'
-import axios from 'axios';
+import axios from 'axios'
+
+
+
+// api key = e442f5514007c831d3a0b321aabfa580c6f5bf2d5bd5e4a3bf24bc5d28a33ce5 
 
 const Covid = () => {
-  const sendAirtime = async (phoneNo) => {
-    try {
-      const response = await axios.post(
-        'https://api.sandbox.africastalking.com/version1/airtime/send',
-        {
-          username: 'sandbox',
-          recipients: [{ phoneNumber: phoneNo, currencyCode: "KES",
-          amount: 5 }],
-          message: 'Thanks for your support!',
-          enqueue: true
-        },
-        {
-          headers: {
-            'Content-Type': 'application/x-www-form-urlencoded',
-            'apiKey':
-              '8aa287ffda67312cee36e9a29ac1699df63832671812c302d08169c4b63eee4a',
-            'Access-Control-Allow-Origin': '*',
-            'Access-Control-Allow-Methods': 'GET,PUT,POST,DELETE,PATCH,OPTIONS',
-          }
-        },
-        console.log('Success!')
-      );
+  
+// handling submit 
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const phoneNo = e.target.phoneNo.value;
+    // console.log(phoneNo)
 
-      console.log(response.data);
-    } catch (error) {
-      console.error(error);
-    }
-  };
 
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    const phoneNo = event.target.phoneNo.value;
-    console.log(phoneNo)
-    sendAirtime(phoneNo);
-  };
+    const data = {
+      recipients: [
+        {
+          phoneNumber: phoneNo,
+          amount: 300,
+        	currencyCode: 'KES'
+        }
+      ]
+    };
+    
+    const config = {
+      headers: {
+        'Content-Type': 'application/json',
+        'apiKey': 'e442f5514007c831d3a0b321aabfa580c6f5bf2d5bd5e4a3bf24bc5d28a33ce5 '
+      }
+    };
+    
+    axios.post('https://api.sandbox.africastalking.com/version1/airtime/send', data, config)
+      .then(response => {
+        console.log('Success:', response.data);
+      })
+      .catch(error => {
+        console.error('Error:', error);
+      });
+    
+
+
+
+
+
+
+
+
+// // setting up the creds 
+// const credentials = {
+// 	apiKey: ' e442f5514007c831d3a0b321aabfa580c6f5bf2d5bd5e4a3bf24bc5d28a33ce5 ',
+// 	username: 'sandbox'
+// };
+
+
+// const AfricasTalking = require('africastalking')(credentials);
+
+// const credit = AfricasTalking.AIRTIME;
+
+
+// const options = {
+// 	recipients: [
+// 		{
+// 			phoneNumber: phoneNo,
+// 			amount: 300,
+// 			currencyCode: 'KES'
+// 		}
+// 	]
+// };
+
+// credit.send(options)
+//     .then( response => {
+//         console.log(response);
+//     })
+//     .catch( error => {
+//         console.log(error);
+//     });
+
+};
 
   return (
     <>
@@ -46,23 +87,26 @@ const Covid = () => {
           <label htmlFor="name">Name</label>
           <input type="text" name="name" />
 
-          <label htmlFor="natId">National ID</label>
-          <input type="number" name="natId" />
-
+          {/* <label htmlFor="natId">National ID</label>
+          <input type="number" name="natId" /> */}
+{/* 
           <label htmlFor="testNo">Test Number</label>
-          <input type="number" name="testNo" />
+          <input type="number" name="testNo" /> */}
 
           <label htmlFor="phoneNo">Phone Number</label>
           <input type="tel" name="phoneNo" />
-
+          
+{/* 
           <label htmlFor="date">Date</label>
-          <input type="date" name="date" />
+          <input type="date" name="date" /> */}
 
           <button type="submit">Submit</button>
         </form>
       </div>
     </>
   );
+
+
 };
 
 export default Covid;
